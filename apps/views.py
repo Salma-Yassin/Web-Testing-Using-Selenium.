4178e680d6b0924145fd3f709d4df73295229e3e
Salma-Yassin <s-salma.mahran@zewailcity.edu.eg>
2023-05-20 23:12:41 +0300

    Add some modifications

--- apps/views.py
+++ apps/views.py
@@ -5,7 +5,6 @@ Copyright (c) 2019 - present AppSeed.us
 
 # Flask modules
 from flask   import Flask, render_template, request, flash, redirect, url_for, jsonify
-import json
 from jinja2  import TemplateNotFound
 from .models import *
 from flask_login import login_user, login_required, current_user, logout_user
@@ -13,17 +12,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
-import os
 from wtforms.validators import InputRequired
 from random import sample 
-
-import sys
 from .controller import controller
-
-# App modules
 from apps import app
-import random
-from datetime import datetime, timedelta
 
 import re
 
@@ -110,43 +102,6 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for('login'))
-
-# General route 
-@app.route('/<template>')
-@login_required
-def route_template(template):
-
-    try:
-
-        if not template.endswith('.html'):
-            template += '.html'
-
-        # Detect the current page
-        segment = get_segment(request)
-
-        # Serve the file (if exists) from app/templates/home/FILE.html
-        return render_template("home/" + template, segment=segment)
-
-    except TemplateNotFound:
-        return render_template('home/page-404.html'), 404
-
-    except:
-        return render_template('home/page-500.html'), 500
-
-# Helper - Extract current page name from request
-def get_segment(request):
-
-    try:
-
-        segment = request.path.split('/')[-1]
-
-        if segment == '':
-            segment = 'index'
-
-        return segment
-
-    except:
-        return None
     
 
 @app.route('/index')
